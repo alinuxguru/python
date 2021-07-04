9dc7e5eb5d0a2b4954b66a48bc85e8918bc91539
Santhakumar <30716757+alinuxguru@users.noreply.github.com>
2021-07-04 23:28:34 +0530

    Create f_string.py
    instead of convert each to str, we can use f - string to convert all to str.

---
+++ f_string.py
@@ -0,0 +1,5 @@
+score = 100
+height = 1.8
+isWinning = True
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
+your score is 100, your height is 1.8, you are winning is True
