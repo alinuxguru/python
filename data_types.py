72c9f48211dbff10ed836a4fc2d5d74f2ddef2f3
Santhakumar <30716757+alinuxguru@users.noreply.github.com>
2021-07-04 22:35:37 +0530

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,13 @@
+print("Hello"[0])
+print("Hello"[2])
+print("123" + "456")
+print("Santh" + "132434")
+print("Santh" + 132434) ## error in string concatenate, since integer
+print(1234 + 1445)
+num = 734_529.678 ## _ for readable format, still consider as float by .
+print(num)
+print(type(num)) ## Print type of data type , float
+print(70 + float("100.50")) ##17.50
+print(str(70) + str(100)) ##70100
+new_num = str(num)
+print(type(new_num)) ## str type
