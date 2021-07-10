9f14ea9a396375e64ce917a76ba3e13705082560
Santhakumar <30716757+alinuxguru@users.noreply.github.com>
2021-07-10 19:40:03 +0530

    Create random_fn.py
    output:
    
    230
    0.9625982767398559
    2.830673309936821

---
+++ random_fn.py
@@ -0,0 +1,13 @@
+import random
+
+randomInteger = random.randint(1, 1000)
+print(randomInteger)
+
+## Returns the next random floating point number between [0.0 to 1.0)
+randomFloat = random.random()
+print(randomFloat)
+
+## if want to print between 0.0 to 5.0 
+randomFloat = random.random() * 5
+print(randomFloat)
+
