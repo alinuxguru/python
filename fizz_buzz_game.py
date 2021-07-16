9989c2a2e25864108fb514341141e14312a8d24a
Santhakumar <30716757+alinuxguru@users.noreply.github.com>
2021-07-16 20:57:29 +0530

    Create fizz_buzz_game.py
    Your program should print each number from 1 to 100 in turn.
    
    When the number is divisible by 3 then instead of printing the number it should print "Fizz".
    
    `When the number is divisible by 5, then instead of printing the number it should print "Buzz".` 
    `When the number is divisible by 5, then instead of printing the number it should print "Buzz".` 
      `And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"`
      `And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"`
    e.g. it might start off like this:
    
    `1
    2
    Fizz
    4
    Buzz
    Fizz
    7
    8
    Fizz
    Buzz
    11
    Fizz
    13
    14
    FizzBuzz`

---
+++ fizz_buzz_game.py
@@ -0,0 +1,9 @@
+for z in range(1,101):
+  if z % 3 == 0 and z % 5 == 0:
+    print("FizzBuzz")
+  elif z % 3 == 0:
+    print("Fizz")  
+  elif z % 5 == 0:
+    print("Buzz") 
+  else:
+    print(z)
