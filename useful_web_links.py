7c45428e96408b40780dfdfc5bc1ac63e77d9afa
Santhakumar <30716757+alinuxguru@users.noreply.github.com>
2021-07-17 21:45:30 +0530

    Update useful_web_links.py

--- useful_web_links.py
+++ useful_web_links.py
@@ -1,7 +1,9 @@
 # ACII ART
 https://ascii.co.uk/art
   
-  
+# To teach Python / or learn python
+  https://reeborg.ca/reeborg.html
+    
 # In built function Robot 
   https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
     
