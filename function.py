591b54be5c933d93666da9872a5f59e3b67b65aa
Santhakumar <30716757+alinuxguru@users.noreply.github.com>
2021-07-22 08:04:02 +0530

    Update function.py

--- function.py
+++ function.py
@@ -19,3 +19,11 @@ greet_with("Santhakumar", "Kumbakonam")
 
 # parameter is name of arguments, here 'name' is a parameter
 # Arguments is actual one, Santhakumar is an argument
+
+
+#function with keyword arguments
+def greet_with(name, location):
+  print(f"Hi, {name} ")
+  print(f"Do you like this place, {location}")
+
+greet_with(name="Sathakumar", location="Kudandhai") ## name="Santhakumar" is a keyword argument
