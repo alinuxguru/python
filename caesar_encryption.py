2a47c8ca229cd9b97eaa78d74073c4a1f4809ce8
Santhakumar <30716757+alinuxguru@users.noreply.github.com>
2021-07-23 16:34:47 +0530

    Create caesar_encryption.py

---
+++ caesar_encryption.py
@@ -0,0 +1,26 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+encoded = []
+def encrypt(text, shift):
+  for z in text:
+    indexx = (alphabet.index(z)) + shift
+    encoded.append(alphabet[indexx])
+
+  print(f"{''.join(encoded)}")
+
+
+
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+encrypt(text, shift)
