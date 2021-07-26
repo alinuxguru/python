198763870ef342ff3a433e37a8f47918c93413f2
Santhakumar <30716757+alinuxguru@users.noreply.github.com>
2021-07-26 21:16:20 +0530

    Update return_function.py
    Santhakumar Palraj
    What is your first name?: 
    What is your last name?: 
    You didn't provide valid inputs

--- return_function.py
+++ return_function.py
@@ -1,4 +1,7 @@
 def reprint_my_name(first_name, last_name):
+  if first_name == '' or last_name == '':
+    return "You didn't provide valid inputs"
+ 
   f_name = first_name.title() 
   l_name = last_name.title()
   return f"{f_name} {l_name}"
@@ -6,4 +9,4 @@ def reprint_my_name(first_name, last_name):
 
 output = reprint_my_name("Santhakumar", "palraj")
 print(output)
-print(reprint_my_name("Santhakumar", "pAlrAJ"))
+print(reprint_my_name(input("What is your first name?: "), input("What is your last name?: ")))
