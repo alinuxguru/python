e735ad1ba1b7efdefda2d977f52229b4607f9991
Santhakumar <30716757+alinuxguru@users.noreply.github.com>
2021-07-30 04:52:52 +0530

    Create docstring_in_python.py
    in variable  a = """ I'm a dotcor"""

---
+++ docstring_in_python.py
@@ -0,0 +1,12 @@
+def my_fn(a):
+    """ It's just a testing code,
+    to check the knowldge, i've written inside docstring"""    
+    if a < 40:
+        return 
+        print("Terrible")
+    if a < 80:
+        return "Pass"
+    else:        
+        return "Great"
+
+print(my_fn(25))        
