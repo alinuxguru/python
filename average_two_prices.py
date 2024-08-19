1cca949d32d92d2beab20dc22858ab4d12e43652
Santhakumar <30716757+alinuxguru@users.noreply.github.com>
2024-08-19 19:40:50 +0530

    Create average_two_prices.py

---
+++ average_two_prices.py
@@ -0,0 +1,7 @@
+## average of two inputs
+price1 = float(input("price1="))
+quantity1 = float(input("quantity="))
+price2 = float(input("price2="))
+quantity2 = float(input("quantity2="))
+averagge = ((price1 * quantity1) + (price2 * quantity2)) / (quantity1 + quantity2)
+print(averagge)
